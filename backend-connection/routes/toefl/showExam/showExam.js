9deff9fe166bc0787cbe68a76fb33be7db963213
incheolha <incheolha@gmail.com>
2018-11-20 23:06:05 +0900

    20118.11.20
    updated

--- backend-connection/routes/toefl/showExam/showExam.js
+++ backend-connection/routes/toefl/showExam/showExam.js
@@ -4,21 +4,22 @@ const router = express.Router();
 // 결제 http request시 salt key는 없지만
 // checkAuth를 통해 로그인이 되어있는지 체크해봐야 한다.
 const jwt = require('jsonwebtoken');
+const saltKey = require('../../../middleware/saltKey');
 
 const ToeflShowController = require('../../../controllers/toefl/showExam/showExam');
 
 router.get('/', ToeflShowController.show_get_all);
 
 router.use('/', (req, res, next) => {
-    jwt.verify(req.query.token, (err, decoded) => {
-        if(err) {
-            res.status(401).json({
-                title: 'auth failed',
-                error: err
-            });
-        }
-        next();
-    });
+  jwt.verify(req.query.token, saltKey, (err, decoded) => {
+      if (err) {
+          res.status(401).json({
+              title: 'Auth Failed',
+              error: err
+          });
+      }
+      next();
+  });
 });
 
 router.post('/:toeflNo', ToeflShowController.show_payment_exam);
